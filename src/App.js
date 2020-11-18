201dd5ccedc724ba307ee5e9ce06c2496ee19049
Eshan-Agarwal <eshanagarwal12@gmail.com>
2020-11-18 23:27:46 +0530

    update heroku server links

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://salty-taiga-19938.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -126,7 +126,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://salty-taiga-19938.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
